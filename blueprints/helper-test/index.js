af0b56519fe402974423b0f8905178f299d7758f
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-03-08 12:07:59 +0100

    Import blueprints from ember.js
    see https://github.com/emberjs/ember.js/pull/13029

--- blueprints/helper-test/index.js
+++ blueprints/helper-test/index.js
@@ -1,14 +1,15 @@
 /*jshint node:true*/
 
-var getDependencyDepth = require('ember-cli-get-dependency-depth');
 var testInfo = require('ember-cli-test-info');
+var stringUtils = require('ember-cli-string-utils');
+var useTestFrameworkDetector = require('../test-framework-detector');
 
-module.exports = {
+module.exports = useTestFrameworkDetector({
   description: 'Generates a helper unit test.',
   locals: function(options) {
     return {
-      friendlyTestName: testInfo.name(options.entity.name, "Unit", "Helper"),
-      dependencyDepth: getDependencyDepth(options.entity.name)
+      friendlyTestName: testInfo.name(options.entity.name, 'Unit', 'Helper'),
+      dasherizedModulePrefix: stringUtils.dasherize(options.project.config().modulePrefix)
     };
   }
-};
+});
--- blueprints/route/index.js
+++ blueprints/route/index.js
@@ -6,7 +6,7 @@ var chalk       = require('chalk');
 var EmberRouterGenerator = require('ember-router-generator');
 
 module.exports = {
-  description: 'Generates a route and registers it with the router.',
+  description: 'Generates a route and a template, and registers the route with the router.',
 
   availableOptions: [
     {
@@ -18,6 +18,10 @@ module.exports = {
       name: 'skip-router',
       type: Boolean,
       default: false
+    },
+    {
+      name: 'reset-namespace',
+      type: Boolean
     }
   ],
 
