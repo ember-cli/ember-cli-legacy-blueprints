ea8c90b34d5f8a4e5dfba46397fdd0f3543c0b1c
Tobias Bieniek <tobias.bieniek@gmail.com>
2016-04-06 19:59:57 +0200

    tests/serializer: Use alternative blueprint test helpers

--- node-tests/blueprints/serializer-test.js
+++ node-tests/blueprints/serializer-test.js
@@ -1,63 +1,56 @@
 'use strict';
 
-var setupTestHooks     = require('ember-cli-blueprint-test-helpers/lib/helpers/setup');
-var BlueprintHelpers   = require('ember-cli-blueprint-test-helpers/lib/helpers/blueprint-helper');
-var generateAndDestroy = BlueprintHelpers.generateAndDestroy;
+var blueprintHelpers = require('ember-cli-blueprint-test-helpers/helpers');
+var setupTestHooks = blueprintHelpers.setupTestHooks;
+var emberNew = blueprintHelpers.emberNew;
+var emberGenerateDestroy = blueprintHelpers.emberGenerateDestroy;
+var modifyPackages = blueprintHelpers.modifyPackages;
+
+var chai = require('ember-cli-blueprint-test-helpers/chai');
+var expect = chai.expect;
 
 describe('Acceptance: ember generate and destroy serializer', function() {
   setupTestHooks(this);
 
   it('serializer', function() {
-    return generateAndDestroy(['serializer', 'foo'], {
-      files: [
-        {
-          file: 'app/serializers/foo.js',
-          contains: [
-            'import JSONAPISerializer from \'ember-data/serializers/json-api\';',
-            'export default JSONAPISerializer.extend('
-          ]
-        },
-        {
-          file: 'tests/unit/serializers/foo-test.js',
-          contains: [
-            'moduleForModel(\'foo\''
-          ]
-        }
-      ]
-    });
+    var args = ['serializer', 'foo'];
+
+    return emberNew()
+      .then(() => emberGenerateDestroy(args, _file => {
+        expect(_file('app/serializers/foo.js'))
+          .to.contain('import JSONAPISerializer from \'ember-data/serializers/json-api\';')
+          .to.contain('export default JSONAPISerializer.extend(');
+
+        expect(_file('tests/unit/serializers/foo-test.js'))
+          .to.contain('moduleForModel(\'foo\'');
+      }));
   });
 
   it('serializer-test', function() {
-    return generateAndDestroy(['serializer-test', 'foo'], {
-      files: [
-        {
-          file: 'tests/unit/serializers/foo-test.js',
-          contains: [
-            'moduleForModel(\'foo\''
-          ]
-        }
-      ]
-    });
+    var args = ['serializer-test', 'foo'];
+
+    return emberNew()
+      .then(() => emberGenerateDestroy(args, _file => {
+        expect(_file('tests/unit/serializers/foo-test.js'))
+          .to.contain('moduleForModel(\'foo\'');
+      }));
   });
 
   it('serializer-test for mocha', function() {
-    return generateAndDestroy(['serializer-test', 'foo'], {
-      packages: [
-        { name: 'ember-cli-qunit', delete: true },
-        { name: 'ember-cli-mocha', dev: true }
-      ],
-      files: [
-        {
-          file: 'tests/unit/serializers/foo-test.js',
-          contains: [
-            'import { describeModel, it } from \'ember-mocha\';',
-            'describeModel(\n  \'foo\',',
-            'Unit | Serializer | foo',
-            'needs: [\'serializer:foo\']',
-            'expect(serializedRecord).to.be.ok;'
-          ]
-        }
-      ]
-    });
+    var args = ['serializer-test', 'foo'];
+
+    return emberNew()
+      .then(() => modifyPackages([
+        {name: 'ember-cli-qunit', delete: true},
+        {name: 'ember-cli-mocha', dev: true}
+      ]))
+      .then(() => emberGenerateDestroy(args, _file => {
+        expect(_file('tests/unit/serializers/foo-test.js'))
+          .to.contain('import { describeModel, it } from \'ember-mocha\';')
+          .to.contain('describeModel(\n  \'foo\',')
+          .to.contain('Unit | Serializer | foo')
+          .to.contain('needs: [\'serializer:foo\']')
+          .to.contain('expect(serializedRecord).to.be.ok;');
+      }));
   });
 });
